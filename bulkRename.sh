dba087c5dd43b6c6b7e21e0afededf6674cddbbb
Dmitrii Grizlov <dmitrii.g@onestepgps.com>
2023-06-28 09:36:49 -0700

    Did refactoring, added ability to specify paths and appendix from terminal.

--- bulkRename.sh
+++ bulkRename.sh
@@ -1,4 +1,18 @@
-for file in *; 
+#!/bin/bash
+
+if [ "$#" != 3 ]; then
+    echo "Error: Insufficient number of arguments. Please provide 3 arguments: [path_from] [path_to] [string_to_append]"
+    exit 1
+fi
+
+pathFrom=$1
+pathTo=$2
+stringToAppend=$3
+
+for object in "$pathFrom"/*; 
 do 
-    mv "$file" "../device_icons/${file%.*}_solid.${file##*.}"; 
+    if [ -f "$object" ]; then
+        filename="$(basename -- $object)";
+        mv "$filename" "$pathTo/${filename%.*}$stringToAppend.${filename##*.}"; 
+    fi
 done
